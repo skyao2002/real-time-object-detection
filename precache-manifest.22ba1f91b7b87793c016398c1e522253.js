23df8df27029d35dac7884c8dc6a63e9f41506c6
Steven Yao <51345129+skyao2002@users.noreply.github.com>
2021-01-14 14:23:30 -0600

    Updates

---
+++ precache-manifest.22ba1f91b7b87793c016398c1e522253.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a652b72f22819265a91fd75af5333711",
+    "url": "/real-time-object-detection/index.html"
+  },
+  {
+    "revision": "c7a7ccab8a7a1577a976",
+    "url": "/real-time-object-detection/static/css/main.523f09c5.chunk.css"
+  },
+  {
+    "revision": "099a5863fb45d793d101",
+    "url": "/real-time-object-detection/static/js/2.ba583733.chunk.js"
+  },
+  {
+    "revision": "b183437b48fb0c7c10973acfa520fbc1",
+    "url": "/real-time-object-detection/static/js/2.ba583733.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c7a7ccab8a7a1577a976",
+    "url": "/real-time-object-detection/static/js/main.50d880ba.chunk.js"
+  },
+  {
+    "revision": "45ad2cf9ff4b6727c7c8",
+    "url": "/real-time-object-detection/static/js/runtime-main.5e0ff53e.js"
+  }
+]);
